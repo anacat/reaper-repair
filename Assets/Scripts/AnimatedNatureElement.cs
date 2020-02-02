19a18568f8dc35546847af913431da3fc222ac13
Ana Lopes <catarina.lopes.04@hotmail.com>
2020-02-02 00:30:51 +0000

    Some sprites
    some logic too

---
+++ Assets/Scripts/AnimatedNatureElement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedNatureElement : MonoBehaviour
+{
+    public InputManager.InputButton myButton;
+    public List<Sprite> spriteList;
+    public int state;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        //success
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (state < spriteList.Count)
+            {
+                state++;
+            }
+            else
+            {
+                state--;
+            }
+
+            _spriteRenderer.sprite = spriteList[state - 1];
+        }
+    }
+}
